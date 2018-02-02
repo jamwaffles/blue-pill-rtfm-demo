cab73681270bf2e837c581e2420e8d4f359fbda1
James Waples <jamwaffles@gmail.com>
2018-02-02 21:33:21 +0000

    Add ST-Link/GDB run commands

--- flash.bat
+++ flash.bat
@@ -1,3 +1,2 @@
-xargo build --release --target thumbv7em-none-eabihf
-arm-none-eabi-objcopy -O ihex .\target\thumbv7em-none-eabihf\release\blue-pill-rtfm-demo .\target\thumbv7em-none-eabihf\release\blue-pill-rtfm-demo.ihex
-.\stm32flash-0.5-win64\stm32flash.exe -b 115200 -w .\target\thumbv7em-none-eabihf\release\blue-pill-rtfm-demo.ihex -g 0x8000000 COM7
+xargo build --release
+arm-none-eabi-gdb.exe -iex "set auto-load safe-path /" .\target\thumbv7em-none-eabihf\release\blue-pill-rtfm-demo
