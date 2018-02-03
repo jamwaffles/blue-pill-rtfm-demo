86275753b92f2ec94b1d5d65ddb04dfd68e47467
James Waples <jamwaffles@gmail.com>
2018-02-03 22:44:55 +0000

    Minimal RTFM app with GDB printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
+#![feature(const_fn)]
+#![feature(proc_macro)]
 
 extern crate cortex_m;
+#[macro_use]
+extern crate cortex_m_rtfm as rtfm;
+extern crate cortex_m_rtfm_macros;
 extern crate cortex_m_semihosting as sh;
 // extern crate mpu9250;
 extern crate stm32f103xx_hal as hal;
@@ -9,102 +14,62 @@ extern crate embedded_hal;
 extern crate nb;
 
 use cortex_m::asm;
+use embedded_hal::blocking::spi::Transfer;
+use embedded_hal::spi::{ Mode, Phase, Polarity };
 use hal::delay::Delay;
 use hal::prelude::*;
 use hal::spi::{ Spi };
-use embedded_hal::spi::{ Mode, Phase, Polarity };
 use hal::stm32f103xx;
-use embedded_hal::blocking::spi::Transfer;
 use hal::timer::Timer;
+use cortex_m_rtfm_macros::app;
+use rtfm::{/*app, */Resource, Threshold};
 // use mpu9250::Mpu9250;
 
 use core::fmt::Write;
 
 use sh::hio;
+use sh::hio::{ HStdout };
 
-fn main() {
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32f103xx::Peripherals::take().unwrap();
+// TASKS AND RESOURCES
+app! {
+    device: hal::stm32f103xx,
 
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    resources: {
+        // static BUSY: bool = false;
+        // static CONTEXT_SWITCHES: u16 = 0;
+        // static FRAMES: u8 = 0;
+        // static RGB_ARRAY: Aligned<u32, [u8; 72]> = Aligned([0; 72]);
+        // static RX_BUFFER: Buffer<[u8; 72], Dma1Channel5> = Buffer::new([0; 72]);
+        // static SLEEP_CYCLES: u32 = 0;
+        // static TX_BUFFER: Buffer<[u8; 13], Dma1Channel4> = Buffer::new([0; 13]);
+        // static WS2812B_BUFFER: Buffer<[u8; 577], Dma1Channel2> =
+        //     Buffer::new([0; 577]);
 
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+        static DBG: HStdout;
+    },
 
-    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    idle: {
+        resources: [
+            DBG,
+        ],
+    },
+}
 
-    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+fn init(p: init::Peripherals) -> init::LateResources {
+    let mut hstdout = hio::hstdout().unwrap();
 
-    let nss = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
+    writeln!(hstdout, "Init").unwrap();
 
-    // SPI1
-    let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
-    let miso = gpioa.pa6;
-    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+    init::LateResources {
+        DBG: hstdout,
+    }
+}
 
-    let mut spi = Spi::spi1(
-        dp.SPI1,
-        (sck, miso, mosi),
-        &mut afio.mapr,
-        Mode {
-            polarity: Polarity::IdleLow,
-            phase: Phase::CaptureOnFirstTransition,
-        },
-        6_400_000.hz(),
-        clocks,
-        &mut rcc.apb2,
-    );
+fn idle(t: &mut Threshold, r: idle::Resources) -> ! {
+    // late resources can be used at this point
+    let mut hstdout: &'static mut HStdout = r.DBG;
 
-    let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
-
-    const ON_BYTE: u8 = 0b1111_1100;
-    const OFF_BYTE: u8 = 0b1100_0000;
-
-    // loop {
-        spi.transfer(&mut [
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-            OFF_BYTE,
-            ON_BYTE,
-        ]);
-
-    //     block!(timer.wait()).unwrap();
-    // }
-
-    // loop {
-    //     // spi.send(0b1111_0000);
-    //     spi.transfer(&mut [ 0b1111_0000 ]);
-    // }
-
-    // let mut delay = Delay::new(cp.SYST, clocks);
-
-    // let mut mpu9250 = Mpu9250::new(spi, nss, &mut delay).unwrap();
-
-    // // sanity checks
-    // assert_eq!(mpu9250.who_am_i().unwrap(), 0x71);
-    // assert_eq!(mpu9250.ak8963_who_am_i().unwrap(), 0x48);
-
-    // let _a = mpu9250.all().unwrap();
-
-    asm::bkpt();
+    loop {
+        writeln!(hstdout, "Idle").unwrap();
+    }
 }
