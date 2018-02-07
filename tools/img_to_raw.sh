357f1018006aa32e48d804f316327645ce994e65
James Waples <jamwaffles@gmail.com>
2018-02-07 21:27:21 +0000

    Generalise script to support 1BPP

--- tools/img_to_raw.sh
+++ tools/img_to_raw.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-# Convert a PNG to an 8BPP greyscale image
-convert samuel.png -depth 8 gray:samuel.raw
+# Convert a PNG to a 1BPP greyscale image
+convert $1.png -depth $2 gray:"$1_$2bpp".raw
